0814202c4b28aeacc515567b51343ed3404dea9e
Ubuntu <ubuntu@ip-172-31-28-26.us-east-2.compute.internal>
2021-10-18 11:00:14 +0000

    Terminei, falta testar

--- TrabalhoFinal/no.py
+++ TrabalhoFinal/no.py
@@ -19,6 +19,11 @@ inicio = []
 sucessor = {}
 ate = {}
 porta = {}
+meuid = []
+
+tabelaHash = {}
+ocupado = []
+conteudo = []
 
 def iniciaServidor(HOST, PORT):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -88,7 +93,17 @@ def atendeRequisicoes(clisock, endr):
             time.sleep(1)
             msg = clisock.recv(1024)
             mensagem = str(msg, encoding = 'utf-8')
-            clisock.send(str.encode(str(hash(mensagem))))
+            chavehash = hash(mensagem)
+            aux = 1
+            try:
+                no = tabelaHash[chavehash]
+            except:
+                clisock.send(str.encode('Mensagem não encontrada'))
+                aux = 0
+            if(aux == 1):
+               saida =  buscarinformacao(no)
+               clisock.send(str.encode(str(saida)))
+
         del conexoes[clisock]
         entradas.remove(clisock)
         clisock.close()
@@ -115,23 +130,206 @@ def atendeRequisicoes(clisock, endr):
             time.sleep(1)
             msg = clisock.recv(1024)
             mensagem = str(msg, encoding = 'utf-8')
-            partes = mensagem.split()
-            clisock.send(str.encode(str(hash(partes[0]))))
+            partes = mensagem.split(' ', 1)
+            funcionou = acharDisponivel(partes[1], meuid[0])
+            if (funcionou != -1):
+                atualizarTabelaHash(hash(partes[0]), funcionou, meuid[0])
+                clisock.send(str.encode('Salvo com sucesso'))
+            else:
+                clisock.send(str.encode('Ocorreu um erro'))
+        del conexoes[clisock]
+        entradas.remove(clisock)
+        clisock.close()
+   
+
+    if (dataD == "BuscarInformacao"):
+        if(estado[0] == 0):
+            clisock.send(str.encode('Estou inativo'))
+        else:
+            clisock.send(str.encode('Estou ativo'))
+            time.sleep(1)
+            msg = clisock.recv(1024)
+            no = str(msg, encoding = 'utf-8')
+            saida = buscarinformacao(no)
+            clisock.send(str.encode(str(saida)))
         del conexoes[clisock]
         entradas.remove(clisock)
         clisock.close()
 
 
 
+    if (dataD == "AdicionarHash"):
+        if(estado[0] == 0):
+            clisock.send(str.encode('Estou inativo'))
+        else:
+            clisock.send(str.encode('Estou ativo'))
+            time.sleep(1)
+            msg = clisock.recv(1024)
+            comeco = str(msg, encoding = 'utf-8')
+            clisock.send(str.encode('Continue'))
+            time.sleep(1)
+            msg = clisock.recv(1024)
+            no = str(msg, encoding = 'utf-8')
+            clisock.send(str.encode('Continue'))
+            time.sleep(1)
+            msg = clisock.recv(1024)
+            hashchave = str(msg, encoding = 'utf-8')
+            clisock.send(str.encode('Obrigado'))
+
+            atualizarTabelaHash( hashchave, no,comeco)
+        del conexoes[clisock]
+        entradas.remove(clisock)
+        clisock.close()
+
+
+    if (dataD == "AdicionarNo"):
+        if(estado[0] == 0):
+            clisock.send(str.encode('Estou inativo'))
+        else:
+            clisock.send(str.encode('Estou ativo'))
+            time.sleep(1)
+            msg = clisock.recv(1024)
+            comeco = str(msg, encoding = 'utf-8')
+            if(ocupado[0] == 0):
+                clisock.send(str.encode('Continue'))
+            else:
+                if(int(comeco)==int(inicio[0])):
+                    clisock.send(str.encode('Falhou'))
+                else:
+                    clisock.send(str.encode('Continue'))
+            time.sleep(1)
+            msg = clisock.recv(1024)
+            valor = str(msg, encoding = 'utf-8')
+            if(ocupado[0] == 0):
+                ocupado[0] = 1
+                conteudo.append(valor)
+                clisock.send(str.encode('Funcionou'))
+            else:
+                resultado = acharDisponivel(valor, comeco)
+                if(resultado == 1):
+                    clisock.send(str.encode(str(meuid[0])))
+                else:
+                    clisock.send(str.encode('Falhou'))
+        del conexoes[clisock]
+        entradas.remove(clisock)
+        clisock.close()
+
 
     if dataD == "exit":
         return 1
     return 0
 
+def acharDisponivel(valor, comeco):
+    sockVizinho = socket.socket()
+    sockVizinho.connect(('localhost', int(porta[int(inicio[0])])))
+    sockVizinho.send(str.encode('AdicionarNo'))
+    time.sleep(1)
+    msg = sockVizinho.recv(1024)
+    status = str(msg, encoding = 'utf-8')
+    if (status == 'Estou ativo'):
+        sockVizinho.send(str.encode(str(comeco)))
+        time.sleep(1)
+        msg = clisock.recv(1024)
+        feedback = str(msg, encoding = 'utf-8')
+        if(feedback == 'Continue'):
+            sockVizinho.send(str.encode(str(valor)))
+            time.sleep(1)
+            msg = clisock.recv(1024)
+            resultado = str(msg, encoding = 'utf-8')
+            if(resultado != 'Falhou'):
+                sockVizinho.close()
+                return resultado
+            else:
+                sockVizinho.close()
+                return -1
+        else:
+            sockVizinho.close()
+            return -1
+    else:
+        sockVizinho.close()
+        return -1
+
+def atualizarTabelaHash( hashchave, no,comeco):
+    tabelaHash[int(no)] = str(hashchave)
+    sockVizinho = socket.socket()
+    sockVizinho.connect(('localhost', int(porta[int(inicio[0])])))
+    sockVizinho.send(str.encode('AdicionarHash'))
+    time.sleep(1)
+    msg = clisock.recv(1024)
+    if (status == 'Estou ativo'):
+        sockVizinho.send(str.encode(str(comeco)))
+        time.sleep(1)
+        msg = clisock.recv(1024)
+        feedback = str(msg, encoding = 'utf-8')
+        sockVizinho.send(str.encode(str(no)))
+        time.sleep(1)
+        msg = clisock.recv(1024)
+        feedback2 = str(msg, encoding = 'utf-8')
+        sockVizinho.send(str.encode(str(hashchave)))
+        time.sleep(1)
+        msg = clisock.recv(1024)
+        feedback3 = str(msg, encoding = 'utf-8')
+        sockVizinho.close()
+
+
+def buscarinformacao(no):
+    ultimo = 0
+    if(int(meuid[0]) == int(no):
+        return conteudo[0]
+    for i in inicio:
+        if(int(sucessor(int(i)))==int(no)):
+            sockVizinho = socket.socket()
+            sockVizinho.connect(('localhost', int(porta[int(i)])))
+            sockVizinho.send(str.encode('BuscarInformacao'))
+            time.sleep(1)
+            msg = clisock.recv(1024)
+            status = str(msg, encoding = 'utf-8')
+            if (status == 'Estou ativo'):
+                sockVizinho.send(str.encode(str(no)))
+                time.sleep(1)
+                msg = clisock.recv(1024)
+                saida = str(msg, encoding = 'utf-8')
+                sockVizinho.close()
+                return saida
+            else:
+                return "Error"
+        else:
+            if((int(no) >= int(i) and no<=ate[int(i)]) or (int(i) >= ate[int(i)] and (int(no) >= int(i) or int(no <= ate[int(i)])))):
+                sockVizinho = socket.socket()
+                sockVizinho.connect(('localhost', int(porta[int(i)])))
+                sockVizinho.send(str.encode('BuscarInformacao'))
+                time.sleep(1)
+                msg = clisock.recv(1024)
+                status = str(msg, encoding = 'utf-8')
+                if (status == 'Estou ativo'):
+                    sockVizinho.send(str.encode(str(no)))
+                    time.sleep(1)
+                    msg = clisock.recv(1024)
+                    saida = str(msg, encoding = 'utf-8')
+                    sockVizinho.close()
+                    return saida
+                return "Error"
+        ultimo = int(i)
+    sockVizinho = socket.socket()
+    sockVizinho.connect(('localhost', int(porta[int(i)])))
+    sockVizinho.send(str.encode('BuscarInformacao'))
+    time.sleep(1)
+    msg = clisock.recv(1024)
+    status = str(msg, encoding = 'utf-8')
+        if (status == 'Estou ativo'):
+            sockVizinho.send(str.encode(str(no)))
+            time.sleep(1)
+            msg = clisock.recv(1024)
+            saida = str(msg, encoding = 'utf-8')
+            sockVizinho.close()
+            return saida
+        else:
+            return "Error"
+
 
 
 def main():
-    id = int(sys.argv[1])
+    meuid.append(int(sys.argv[1]))
     PORT = int(sys.argv[2])
     tamanho = int(sys.argv[3])
     estado.append(0)
